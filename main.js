75d6e30c7500a7ac3697165d3d43920410035026
hamiinimii <Ryuk@Victoria-MacBook-Pro.local>
2018-04-15 17:29:00 +0900

    barIcon modified

--- main.js
+++ main.js
@@ -4,14 +4,27 @@
 //nodeの書き方。requireは、括弧内のものを読み込み、宣言したオブジェクトに格納する。
 //ここでは「electron」というnodeのモジュールを読み込む。
 //読み込みはモジュールだけじゃなく、括弧内に「'./hoge.txt'」などを書けばそのファイルを読み込むことができる。
-const {app, BrowserWindow} = require('electron')
+//const {app, BrowserWindow} = require('electron');
+//const Tray = require('tray');
+
+
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+const Tray = electron.Tray
+
 
 //オブジェクトが勝手に破棄されないように、空オブジェクトのグローバル変数を宣言します。
 let win
+let barIcon
 
 function createWindow () {
   // Windowsのサイズを決めて、オブジェクト生成する。
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    'width': 800,
+    'height': 600
+  })
 
   // 同じ階層にいるindex.htmlを読み込む
   win.loadURL(`file://${__dirname}/index.html`)
@@ -28,6 +41,16 @@ function createWindow () {
 
 // Electronの初期化が完了し、ブラウザウインドウの作成準備が完了したときに呼び出される。
 app.on('ready', createWindow)
+
+//ステータスバー等の通知エリアでのアイコンを作成
+app.on('ready',() => {
+  barIcon = new Tray(__dirname +'/images/icon.png');
+  let contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio'}
+  ])
+  barIcon.setToolTip('This is my application.')
+  barIcon.setContextMenu(contextMenu)
+})
 
 // 全てのウィンドウが閉じられた時の処理
 app.on('window-all-closed', () => {
