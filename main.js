e7b70e4191cccb9c838873ea64283751f9279ad7
hamiinimii <Ryuk@Victoria-MacBook-Pro.local>
2018-04-20 20:12:46 +0900

    some codes discarded

--- main.js
+++ main.js
@@ -5,37 +5,13 @@
 // -src/index.html
 //  -//view.js
 
-//nodeの書き方。requireは、括弧内のものを読み込み、宣言したオブジェクトに格納する。ここでは「electron」というnodeのモジュールを読み込む。
-//読み込みはモジュールだけじゃなく、括弧内に「'./hoge.txt'」などを書けばそのファイルを読み込むことができる。
 const {app,BrowserWindow,dialog,ipcMain,Menu,Tray} = require('electron')
-
-// const app = electron.app
-// const BrowserWindow = electron.BrowserWindow
-// const Menu = electron.Menu
-// const Tray = electron.Tray
-// const ipcMain = electron.ipcMain
 
 //オブジェクトが勝手に破棄されないように、空オブジェクトのグローバル変数を宣言します。
 let win;
 let settingsWin;
 let appIcon;
 let backgroundColor = 'skyblue';
-
-//Menuのテンプレートを作り、呼び出す。
-let menuTemplate =[{
-  label: 'MyApp' , //第一項はアプリ名になる
-  submenu: [
-    {label: 'About', accelerator: 'CmdOrCtrl+Shift+A', click:
-    function(){showAboutDialog();}},
-    {type: 'separator'},
-    {label: 'Settings', accelerator: 'CmdOrCtrl+,', click:
-    function(){showSettingsWindow();}},
-    {type: 'separator'},
-    {label: 'Quit', accelerator: 'CmdOrCtrl+Q', click:
-    function(){app.quit(); }}
-  ]
-}];
-let menu = Menu.buildFromTemplate(menuTemplate);
 
 //ipcによるやりとり。処理の引数はイベント及びその値
 // ipcMain.on('settings_changed', function(event, color){ //onは変化を取得
@@ -50,10 +26,6 @@ ipcMain.on('settings_changed', function(event, color){ //onは変化を取得
 //   event.returnValue = backgroundColor; //
 // });
 // ここでconsole.logしたらターミナルの方に行った
-ipcMain.on('clicked', function(event){
-  console.log('ACHIEVED');
-  appIcon = Tray(__dirname +'/images/icon2.png');
-})
 
 //functions
 let createWindow =()=> {
@@ -64,24 +36,6 @@ let createWindow =()=> {
 
   win.on('closed', () => {win = null}) // 表示したWindowを閉じたときの初期化処理
 }
-
-let showAboutDialog =()=>{
-  dialog.showMessageBox({
-    type: 'info',
-    buttons: ['OK'],
-    message: 'About This App',
-    detail: 'This app was created by hamiinimii'
-  })
-}
-
-let showSettingsWindow =()=> {
-  settingsWin = new BrowserWindow({'width': 800,'height': 600});
-  settingsWin.loadURL(`file://${__dirname}/src/settings.html`);
-  settingsWin.webContents.openDevTools();
-  settingsWin.show();
-  settingsWin.on('closed', () => {settingsWin = null});
-}
-
 
 // Electronの初期化が完了し、ブラウザウインドウの作成準備が完了したときに呼び出される。
 app.on('ready', createWindow)
@@ -98,17 +52,6 @@ app.on('ready',() => {
 
 // 全てのウィンドウが閉じられた時の処理
 app.on('window-all-closed', () => {
-  // macOS(darwin)の場合、全てのウィンドウが閉じてもappを生かすことができる。
-  //if (process.platform !== 'darwin') {
   app.quit()
   //}
 })
-
-/*
-app.on('activate', () => {
-  // macOS：ドックアイコンをクリックした際にウィンドウを再生成する。（ウィンドウなしでappが生きている場合）
-  if (win === null) {
-    createWindow()
-  }
-})
-*/
