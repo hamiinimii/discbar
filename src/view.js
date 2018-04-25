d46efeb84d413433106912da9e8144939314063e
hamiinimii <Ryuk@Victoria-MacBook-Pro.local>
2018-04-25 15:52:50 +0900

    MutationObserever does not work

--- src/view.js
+++ src/view.js
@@ -3,22 +3,21 @@
 //レンダープロセス用のipcモジュールをインポート
 const {ipcRenderer} = require('electron');
 
-window.addEventListener('click', function(){
-  let iconName = document.querySelector("link[rel='icon']").getAttribute('href').slice(70,73);
+// window.addEventListener('click', function(){
+//   // let iconName = document.querySelector("link[rel='icon']").getAttribute('href').slice(70,73);
+//   let iconName = document.getElementById('quote').innerHTML;
+//   ipcRenderer.send('retrieved', iconName);
+// });
+
+//MutationObserverによる監視
+// let icon = document.querySelector("link[rel='icon']");
+let icon = document.getElementById('quote');
+
+const observer = new MutationObserver((MutationRecords) => {
+  let iconName = MutationRecords.target;
+  alert('iconName');
   ipcRenderer.send('retrieved', iconName);
 });
 
-// let iconName = {}
-// document.querySelector("link[rel='icon']").getAttribute('href').slice(70,73);
-// const handler = {
-//   set(target,value){
-//     ipcRenderer.send('retrieved', value);
-//   },
-// };
-// let p = new Proxy(iconName, handler);
-
-// onload = function(){
-//   let unreadCount = document.getElementById('quote').innerHTML;
-//   // let unreadCount = document.getElementsByTagName('link').innerHTML;
-//   ipcRenderer.sendToHost('retrieved', unreadCount);
-// }
+const config = {childList :true, characterData: true};
+observer.observe(icon, config);
