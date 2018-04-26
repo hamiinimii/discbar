7e3391d08c23f5ea4fd9ba7fb4dc569c081defff
hamiinimii <Ryuk@Victoria-MacBook-Pro.local>
2018-04-26 18:28:18 +0900

    view.js document method problems

--- src/view.js
+++ src/view.js
@@ -2,20 +2,21 @@
 
 //レンダープロセス用のipcモジュールをインポート
 const {ipcRenderer} = require('electron');
+let icon = window.document;
 
 // window.addEventListener('click', function(){
 //   // let iconName = document.querySelector("link[rel='icon']").getAttribute('href').slice(70,73);
 //   let iconName = document.getElementById('quote').innerHTML;
+//   alert(icon.getElementById('quote').innerHTML);
 //   ipcRenderer.send('retrieved', iconName);
 // });
 
 //MutationObserverによる監視
 // let icon = document.querySelector("link[rel='icon']");
-let icon = document.getElementById('quote');
 
 const observer = new MutationObserver((MutationRecords) => {
   let iconName = MutationRecords.target;
-  alert('iconName');
+  window.alert(icon.getElementById('quote').innerHTML);
   ipcRenderer.send('retrieved', iconName);
 });
 
