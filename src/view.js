8f8e7ea5f3c8c551083f2a2d88d28a28a5752df4
hamiinimii <Ryuk@Victoria-MacBook-Pro.local>
2018-04-27 15:22:17 +0900

    mutationobserever be not activated

--- src/view.js
+++ src/view.js
@@ -2,23 +2,25 @@
 
 //レンダープロセス用のipcモジュールをインポート
 const {ipcRenderer} = require('electron');
-let icon = window.document;
+window.onload = function(){
+  var icon = document.getElementById('quote');
+};
 
-// window.addEventListener('click', function(){
-//   // let iconName = document.querySelector("link[rel='icon']").getAttribute('href').slice(70,73);
-//   let iconName = document.getElementById('quote').innerHTML;
-//   alert(icon.getElementById('quote').innerHTML);
-//   ipcRenderer.send('retrieved', iconName);
-// });
+window.addEventListener('click', function(){
+  // let iconName = document.querySelector("link[rel='icon']").getAttribute('href').slice(70,73);
+  let iconName = document.getElementById('quote').innerHTML;
+  alert(icon);
+  ipcRenderer.send('retrieved', iconName);
+});
 
 //MutationObserverによる監視
 // let icon = document.querySelector("link[rel='icon']");
 
-const observer = new MutationObserver((MutationRecords) => {
-  let iconName = MutationRecords.target;
-  window.alert(icon.getElementById('quote').innerHTML);
-  ipcRenderer.send('retrieved', iconName);
-});
-
-const config = {childList :true, characterData: true};
-observer.observe(icon, config);
+// const observer = new MutationObserver((MutationRecords) => {
+//   let iconName = MutationRecords.target;
+//   alert(icon.getElementById('quote').innerHTML);
+//   ipcRenderer.send('retrieved', iconName);
+// });
+//
+// const config = {childList :true, attributes:true, characterData: true};
+// observer.observe(icon, config);
